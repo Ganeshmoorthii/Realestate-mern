1257991a091b3fe154ef4c88ec9a2c616d65b57e
Ganeshmoorthii <mgmoorthi2004@gmail.com>
2025-10-06 12:16:21 +0530

    Fix: removed submodule and restored client as normal folder

---
+++ client/src/Pages/SignUp.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export default function SignUp() {
+  const [formData, setFormData] = React.useState({});
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const navigate = useNavigate(); 
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(null); 
+
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      console.log(data);
+
+      // Check for HTTP error status or success flag
+      if (!res.ok || data.success === false) {
+        setError(data.message || "Something went wrong!");
+        setLoading(false);
+        return;
+      }
+
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message || "Something went wrong!");
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          className="border p-3 rounded-lg"
+          id="username"
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+          required
+        />
+        <button
+          disabled={loading}
+          type="submit"
+          className="bg-slate-700 text-white p-3 rounded-lg disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/sign-in" className="underline text-blue-600">
+          <span>Sign In</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-700 mt-5">{error}</p>}
+    </div>
+  );
+}
