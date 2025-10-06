92f094fbdf505a2583e3b245f70ff32062a3b115
Ganeshmoorthii <mgmoorthi2004@gmail.com>
2025-10-06 13:41:54 +0530

    complete sign in route API

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -34,14 +34,17 @@ export const signin = async (req, res, next) => {
     if (!validUser) {
       return next(errorHandler(400, "User not found"));
     }
-    const validPassword = await bcrypt.compare(password, validUser.password);
+    const validPassword = await bcrypt.compareSync(
+      password,
+      validUser.password
+    );
     if (!validPassword) {
-      return next(errorHandler(400, "Invalid password"));
+      return next(errorHandler(401, "Invalid password"));
     }
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
-    res.cookie("acess_token", token, { httpOnly: true }).status(200).json(rest);
-  } catch (e) {
-    next(e);
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
